adfb6ff61343e4bfd0dac9caa1fad3cddc3d7194
Jorge Federico Flores <jfflores90@gmail.com>
2017-03-20 19:52:01 -0300

    cambios

--- interoperabilidad/app.js
+++ interoperabilidad/app.js
@@ -32,31 +32,53 @@ app.get('/sectors/:ecosystem/:reparticion',(req,res) => {
 app.post("/receive", (req,res) =>{
 	let body = req.body;
 	let response = {};
-	console.log('RECEIVING -> ', body);
-	copyProperties(body,response).then(() => console.log("RESPUESTA -> ", response));
+	console.log('received -> ', body);
+	console.log('==============================================');
+	console.log('==============================================');
+	console.log('==============================================');
+	copyProperties(body,response).then(() => res.end());
+	console.log("RESPONSE -> ", response)
 //	.then( () => res.json(response));
 });
 
-const copyProperties = (body,response) => {
+
+const copyPropertiesWithURL = (body,response) => {
 	return Promise.all(Object.keys(body).map( property => {
-			if(body[property].constructor === Array){
+			if(body[property] !== undefined && body[property] !== null &&
+				body[property].constructor === Array){
 				return getContentFromList(body[property],response,property);
 			}else{
-				return getContent(body[property],response,property);
+				return getContent(response,property,body);
 			}
-		}));
-	}
+	}));
+}
 
-function getContent(url,response,property){
+const copyProperties = (body,response) =>{
+	let objWithUrls = undefined;
+	Object.keys(body).forEach( property => {
+		if(typeof body[property] !== 'object' ||  body[property] === null ||
+			body[property] === undefined){
+			response[property] = body[property];
+		}else{
+			objWithUrls = body[property];
+			prop = property;
+			response[property.toString()] = {};
+		}
+	});
+	return copyPropertiesWithURL(objWithUrls,response[prop]);
+}
+
+
+function getContent(response,property,body){
 	return new Promise((resolve,reject) => {
-		http.get(url, res => {
-				let json = '';
-				res.on('data', data => json += data );
-			 	res.on('end', _ => { 
-			 		response[property.toString()] = toJson(json);
-			 		resolve(response);
-				});
+		http.get(body[property], res => {
+			let json = '';
+			res.on('data', data => json += data );
+			 res.on('end', _ => { 
+			 	response[property.toString()] = toJson(json);
+			 	resolve(response);
 			});
+		});
 	});
 }
 
@@ -86,29 +108,36 @@ function toJson(str){
 	return str;
 }
 
+
 server.listen(5000);
 
 
 
+let test = { destino: 'destino',
+  ecosistemaOrigen: 'ecosistemaorigen',
+  ecosistemaDestino: 'ecosistemadestino',
+  estadoSeleccionado: null,
+  pasePropietario: false,
+  expedienteInteroperable:
+   { expediente: 'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN',
+     trata: 'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/trata',
+     documentos:
+      [ 'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/IF-2017-00112909-APN-MARI',
+        'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/IF-2017-00099520-APN-MARI',
+        'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/PV-2017-00126549-APN-MARI',
+        'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/PV-2017-00129486-APN-PRUEBASEE' ],
+     historial: 'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/historial',
+     archivosDeTrabajo:
+      [ 'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/rest.txt/archivoDeTrabajo',
+        'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/ABATE.png/archivoDeTrabajo' ] } }
 
 
-/*
-let test = {
-		expediente:'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN',
-		historial:'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/historial',
-		trata:'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/trata',
-		documentos:
-	['http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/IF-2017-00099520-APN-MARI',
-	'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/112908/APN/documento/IF-2017-00112909-APN-MARI'],
-	archivoDeTrabajo: ['http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/00112908/APN/archivoDeTrabajo/rest.txt',
-					  'http://localhost:8054/gde-restfull-api-web/interoperabilidad/expediente/2017/00112908/APN/archivoDeTranajo/ABATE.png']
-	};
-console.log("ENTRADA ->", test);
 
 
 // It receives a Json of URI's and return a JSON with the result of all invocations //
-
-copyProperties(test,{}).then( arr =>{
+let res = {};
+copyProperties(test,res).then( _ =>{
 	console.log("RESULTADO -> ");
-	console.log(arr[0])
-});*/
+	console.log(res);
+	console.log(res.expedienteInteroperable.historial);
+});
